b70ff15ed3c7a4a273ab9a5671e1ef871515088a
unknown <ljsuderman7@gmail.com>
2020-02-13 11:02:47 -0500

    completed unit tests

---
+++ PROG2070Assignment1.Tests/CircleTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PROG2070Assignment1.Tests
+{
+    [TestFixture]
+    public class CircleTest
+    {
+        Circle circle = new Circle();
+        [Test]
+        public void GetRadius_ResultIs1()
+        {
+            Assert.AreEqual(1, circle.GetRadius());
+        }
+        [Test]
+        public void SetRadius_Given2_ResultIs2()
+        {
+            int radius = 2;
+            circle.SetRadius(radius);
+            Assert.AreEqual(2, circle.GetRadius());
+        }
+        [Test]
+        public void GetCircumference_ResultIs2()
+        {
+            double circumference = 0;
+            circumference = circle.GetCircumference();
+            Assert.AreEqual(2, circumference);
+        }
+
+        [Test]
+        public void GetArea_ResultIsPI()
+        {
+            double area = 0;
+            area = circle.GetArea();
+            Assert.AreEqual(Math.PI, area);
+        }
+    }
+}
