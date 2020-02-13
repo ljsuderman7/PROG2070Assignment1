9578df30eab830c1b8d98834aa8a74a7a8e52b9b
unknown <ljsuderman7@gmail.com>
2020-02-13 10:55:53 -0500

    Circle Class Added

---
+++ PROG2070Assignment1/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assignment1
+{
+    public class Circle
+    {
+        private int radius;
+        public Circle()
+        {
+            radius = 1;
+        }
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+            return (double)radius * 2;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
